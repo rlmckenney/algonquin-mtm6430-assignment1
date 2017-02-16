6f60d19019617d1acfd4bd2039fd3fa3212133ab
Robert McKenney <robert@bidfoundry.com>
2017-02-16 12:38:32 -0500

    Optimized movie-db-service

--- app/core/movie-db/movie-db.service.js
+++ app/core/movie-db/movie-db.service.js
@@ -2,44 +2,16 @@ angular
   .module('core.movieDb')
   .service('movieDb', function ($http) {
 
-    getAll = function() {
-      return $http.get('/data/movies.json')
-        .then(function(response) {
-          return response.data
-        })
-    }
-
-    // getAll = () => ( $http.get('/data/movies.json').then((response) => (response.data)) )
-
-    // getAll = (function(response) {
-    //   return $http.get('/data/movies.json')
-    //     .then(function(response) {
-    //       return response.data
-    //     })
-    // })()
+    getAll = () => ( $http.get('/data/movies.json').then((response) => (response.data)) )
 
     getById = function(itemId) {
-      return $http.get('/data/movies.json')
-        .then(function(response) {
-          return response.data.filter(function(row) {
-            return row.id == itemId // Filter out the appropriate one
-          })[0]
-        })
+      return this.getAll().then( (data) => (data.filter( (row) => (row.id == itemId))[0]) )
     }
 
-    // getById = function(itemId) {
-    //   return this.getAll()
-    //     .then(function(data) {
-    //       return data.filter(function(row) {
-    //         return row.id == itemId // Filter out the appropriate one
-    //       })[0] // because filter always returns a new array and we only want the first object
-    //     })
-    // }
-
     getMaxId = function() {
-      return this.getAll().then(function(data) {
-        return Math.max.apply(Math,data.map(function(movie) {return movie.id}))
-      })
+      return this.getAll().then(
+        (data) => (Math.max.apply(Math,data.map((movie) => (movie.id))))
+      )
     }
 
     return {
