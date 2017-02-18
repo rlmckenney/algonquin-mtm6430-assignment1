a0c755d18f1f7376af3d315caea33474c01b309d
Robert McKenney <robert@bidfoundry.com>
2017-02-18 17:44:57 -0500

    Inserted tutorial documentation

--- app/movie-details/movie-details.component.js
+++ app/movie-details/movie-details.component.js
@@ -1,26 +1,31 @@
+// Notice that because components operate in an isoltated scope, we assign model properties to
+// the controller directly via "this" rather than to an imported $scope object.
 angular
   .module('movieDetails')
   .component('movieDetails', {
     templateUrl: 'movie-details/movie-details.template.html',
     controller: ['$routeParams', '$location', 'movieDb',
       function MovieListCtrl($routeParams, $location, movieDb) {
-        var self = this
+        var self = this  // we are making a referential copy of this (the controller) so that
+                         // we can reference it correctly inside our functions. If we just used
+                         // "this" inside our functions it would refer to the function
+                         // and not our controller.
+
         self.itemId = $routeParams.itemId
 
         movieDb.getById(self.itemId)
-          .then(function(data) {
-            self.movie = data
-          })
+          // the movieDb.getById() function returns a movie object
+          // our .then() function below is implemented with the ES2015 modern javascript notation
+          // and is equivilant to writting:
+          // .then( function(movie) { self.movie = movie } )
+          .then( movie => { self.movie = movie } )
           .catch(function() {
             // if there was an error retrieving the data,
             // redirect to our placeholder
             $location.path('/')
           })
-
-        movieDb.getMaxId()
-          .then(function(data) {
-            self.maxId = data
-          })
+        // the movieDb.getMaxId() function returns the highest id value of all our movie objects
+        movieDb.getMaxId().then( id => { self.maxId = id } )
       }
     ]}
   )
--- app/movie-list/movie-list.component.js
+++ app/movie-list/movie-list.component.js
@@ -1,3 +1,5 @@
+// Notice that because components operate in an isoltated scope, we assign model properties to
+// the controller directly via "this" rather than to an imported $scope object.
 angular
   .module('movieList')
   .component('movieList', {
@@ -5,9 +7,16 @@ angular
     controller: ['$location', 'movieDb',
       function MovieListCtrl($location, movieDb) {
 
-        var self = this;
+        var self = this; // we are making a referential copy of this (the controller) so that
+                         // we can reference it correctly inside our functions. If we just used
+                         // "this" inside our functions it would refer to the function
+                         // and not our controller.
 
         movieDb.getAll().then(function(data) {
+          // We could just pass the whole data array to our movieList property. But, for the sake
+          // of example we are showing how to use the javascript array.map() function to transform
+          // the dataset. In this case we are just extracting selected properties for each object
+          // in the array. We could also have applied some programatic chage to the values.
           self.movieList = data.map(function(movie) {
             return {
               "id": movie.id,
@@ -17,13 +26,21 @@ angular
               "runtime": movie.runtime,
             }
           })
-        }).catch(function(e) {
+        })
+        // This catch() function will only be called if there was an error returned from the
+        // promise resolution in our movieDb.getAll() call. e.g. There was some communication error
+        // in reading the datasource.
+        .catch(function(e) {
           console.error("Error retrieving data from movieDb", e)
-          // set default values in the model ($scope)
+          // set default model properties
           self.movieList = []
           self.maxId = null
         })
-
+        // This function is written in the ES2015 modern javascript standard notation.
+        // It is equivilant to writing:
+        // self.isActive = function (viewLocation) {
+        //   return viewLocation === $location.path()
+        // }
         self.isActive = (viewLocation) => (viewLocation === $location.path())
       }
     ]
